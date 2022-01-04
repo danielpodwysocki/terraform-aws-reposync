dfda0384977a78a502348d6d2309156780522cf2
Daniel Podwysocki <daniel@dapo.tech>
2022-01-04 19:36:28 +0100

    feat: sync the repos with a bucket

--- aws-reposync.sh
+++ aws-reposync.sh
@@ -5,11 +5,17 @@
 #
 # author: @danielpodwysocki (https://gitlab.com/danielpodwysocki)
 # date: 2022-01
-
+echo "Target bucket: $target_bucket"
 sync_path="/tmp/sync"
 repos=$(ls /etc/yum.repos.d)
 echo "Syncing to $sync_path ..."
 mkdir -p $sync_path
+
+echo "Syncing the contents of: $target_bucket to: $sync_path"
+aws s3 sync $target_bucket $sync_path
+
+
+echo "Comparing and syncing repos..."
 
 for repo in $repos
 do
@@ -17,5 +23,7 @@ do
   reposync -p $sync_path --download-metadata --repo=$repo_id
 done
 
+echo "Syncing updated repos to: $target_bucket"
+aws s3 sync $sync_path $target_bucket
 echo "Finished syncing, exiting"
 exit 0
